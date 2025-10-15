63d4f421b08c14bed51f19b85921bb0f62d5b5c6
Daniel Kennett <daniel@cascable.se>
2025-10-15 18:07:16 +0200

    Fix use of deprecated declarations in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [.macOS(.v11), .iOS(.v14), .macCatalyst(.v15), .visionOS("1.1")],
     products: [.library(name: "CascableCoreSimulatedCamera", targets: ["CascableCoreSimulatedCamera"])],
     dependencies: [
-        .package(name: "CascableCore", url: "https://github.com/Cascable/cascablecore-distribution", from: "16.0.1")
+        .package(url: "https://github.com/Cascable/cascablecore-distribution", from: "16.0.1")
     ], targets: [
         .binaryTarget(name: "CascableCoreSimulatedCamera", path: "CascableCoreSimulatedCamera.xcframework")
     ]
